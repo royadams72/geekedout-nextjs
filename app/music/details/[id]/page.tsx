c197aec7ee1cf56df4a65328a31dadadcb6e3770
Roy Adams <roy@eden-grafix.co.uk>
2024-08-05 12:22:21 +0100

    fixed details page loading previous info

--- app/music/details/[id]/page.tsx
+++ app/music/details/[id]/page.tsx
@@ -1,48 +1,131 @@
 "use client";
-// import { handleAlbums } from "@/app/api/music/albums/";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import Image from "next/image";
 
+import { useAppDispatch, useAppSelector } from "@/lib/hooks/store.hooks";
+
+import styles from "@/styles/components/_detail.module.scss";
+import {
+  getAlbum,
+  selectAlbumDetail,
+  selectStatus,
+} from "@/lib/features/music/musicSlice";
+import { StateLoading } from "@/shared/enums/loading";
+import Link from "next/link";
+import { AlbumDetail } from "@/shared/interfaces/music";
+// import { handleAlbums } from "@/app/api/music/albums/";
+const initialAlbumDetail: AlbumDetail = {
+  id: "",
+  artists: [],
+  image: "",
+  name: "",
+  release_date: "",
+  spotify_link: "",
+  tracks: [""],
+  category: "",
+};
 const AlbumDetails = ({ params: { id } }: { params: { id: string } }) => {
-  const [albumDetails, setAlbumDetails] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  const dispatch = useAppDispatch();
+  const [albumDetails, setAlbumDetails] =
+    useState<AlbumDetail>(initialAlbumDetail);
+  const isLoading = useAppSelector(selectStatus) === StateLoading.LOADING;
+  const album: AlbumDetail = useAppSelector(selectAlbumDetail);
+  const [backgroundImage, setBackgroundImage] = useState("");
+  const [category, setCategory] = useState("");
+  const [isFetching, setIsFetching] = useState(true);
+
+  const fetchAlbumDetails = useCallback(() => {
+    setIsFetching(true);
+    dispatch(getAlbum(id));
+  }, [dispatch, id]);
 
   useEffect(() => {
-    const fetchAlbumDetails = async () => {
-      try {
-        // const response = await fetch(`/api/music/get-details?id=${id}`);
-        const response = await fetch(`/api/music/get-details?id=${id}`);
-        console.log(response);
-        const result = await response.json();
-
-        if (response.ok) {
-          setAlbumDetails(result.data);
-        } else {
-          setError(result.error);
-          console.log(response);
-        }
-      } catch (err) {
-        setError("An error occurred while fetching the album details.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
+    setAlbumDetails(initialAlbumDetail);
+    setBackgroundImage("");
+    setCategory("");
     fetchAlbumDetails();
-  }, [id]);
+  }, [fetchAlbumDetails]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+  useEffect(() => {
+    // dispatch(getAlbum(id));
+    console.log(album.id, id);
+
+    if (album && album.id === id) {
+      setAlbumDetails(album);
+      setBackgroundImage(album.image);
+      setCategory(album.category);
+
+      setIsFetching(false);
+    }
+  }, [album, id]);
+
+  const divStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+  };
+
+  if (isLoading || isFetching) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>Album Details</h1>
-      {albumDetails ? (
-        <pre>{JSON.stringify(albumDetails, null, 2)}</pre>
-      ) : (
-        <div>No album details found.</div>
-      )}
-    </div>
+    <>
+      <div className={styles[`details_container_${category?.toLowerCase()}`]}>
+        <div className={styles.details_btn_container}>
+          <Link href={"/music"} className="btn">
+            Back to Music
+          </Link>
+        </div>
+        <div className={styles.details_music}>
+          <div className={styles.details_info}>
+            <h1 className={styles.details_title}>{albumDetails?.name}</h1>
+            {albumDetails?.artists.map((artist, index) => (
+              <h4 key={index} className={styles.details_sub_heading_music}>
+                <Link
+                  href={artist?.spotifyUrl}
+                  className={styles.details_link}
+                  target="_blank"
+                >
+                  {artist.name}
+                </Link>
+              </h4>
+            ))}
+            <h4 className={styles.details_sub_heading_music}>
+              Realease Date: {albumDetails?.release_date}
+            </h4>
+            <h4 className={styles.details_list_heading}>TrackList</h4>
+            <div className={styles.details_ol}>
+              <ol className={styles.details_ol_music}>
+                {albumDetails.tracks.map((track, index) => (
+                  <li key={index} className={styles.details_ol_music_li}>
+                    <span className={styles.det}>{track}</span>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          </div>
+          <div className={styles.details_image}>
+            {albumDetails?.image && (
+              <Image
+                width={300}
+                height={300}
+                src={albumDetails?.image}
+                alt={albumDetails?.name}
+              />
+            )}
+          </div>
+          {albumDetails.spotify_link && (
+            <div>
+              <a
+                href={albumDetails?.spotify_link}
+                target="_blank"
+                className="btn"
+              >
+                Spotify Page
+              </a>
+            </div>
+          )}
+        </div>
+      </div>
+      <div style={divStyle} className={styles.backgroundImage}></div>
+    </>
   );
 };
 
--- lib/features/music/musicSlice.ts
+++ lib/features/music/musicSlice.ts
@@ -1,7 +1,12 @@
 import { createSelector, type PayloadAction } from "@reduxjs/toolkit";
 import { createAppSlice } from "@/lib/createAppSlice";
 import type { AppThunk } from "@/lib/store";
-import { Album, AlbumDetail, MusicStore } from "@/shared/interfaces/music";
+import {
+  Album,
+  AlbumDetail,
+  Artists,
+  MusicStore,
+} from "@/shared/interfaces/music";
 import { StateLoading } from "@/shared/enums/loading";
 import { CategoryType } from "@/shared/enums/category-type.enum";
 import { IMAGE_NOT_FOUND } from "@/shared/enums/image-not-found.enum";
@@ -10,11 +15,13 @@ import { IMAGE_NOT_FOUND } from "@/shared/enums/image-not-found.enum";
 export interface MusicSliceState {
   music: MusicStore;
   status: StateLoading.IDLE | StateLoading.LOADING | StateLoading.FAILED;
+  selectedAlbum: AlbumDetail;
 }
 
 const initialState: MusicSliceState = {
   music: {} as MusicStore,
   status: StateLoading.IDLE,
+  selectedAlbum: {} as AlbumDetail,
 };
 
 // If you are not using async thunks you can use the standalone `createSlice`.
@@ -33,10 +40,10 @@ export const musicSlice = createAppSlice({
     // }),
     getAllAlbums: create.asyncThunk(
       async () => {
-        let data = await getMusic();
-        console.log(data.data.data.albums);
+        let data = await getAllMusic();
+        console.log(data.data.albums);
 
-        return data.data.data.albums;
+        return data.data.albums;
       },
       {
         pending: (state) => {
@@ -51,31 +58,91 @@ export const musicSlice = createAppSlice({
         },
       }
     ),
+    getAlbum: create.asyncThunk(
+      async (id: string) => {
+        let data = await getAlbumDetails(id);
+        // console.log(data);
+
+        return data;
+      },
+      {
+        pending: (state) => {
+          state.status = StateLoading.LOADING;
+        },
+        fulfilled: (state, action) => {
+          state.status = StateLoading.IDLE;
+          state.selectedAlbum = action.payload;
+        },
+        rejected: (state) => {
+          state.status = StateLoading.FAILED;
+        },
+      }
+    ),
   }),
   // You can define your selectors here. These selectors receive the slice
   // state as their first argument.
   selectors: {
     selectStatus: (state) => state.status,
     selectAllAlbums: (state) => state?.music?.items,
+    selectAlbumDetail: (state) => state?.selectedAlbum,
   },
 });
 
 // Action creators are generated for each case reducer function.
-export const { getAllAlbums } = musicSlice.actions;
+export const { getAllAlbums, getAlbum } = musicSlice.actions;
 
 // Selectors returned by `slice.selectors` take the root state as their first argument.
-export const { selectStatus, selectAllAlbums } = musicSlice.selectors;
+export const { selectStatus, selectAllAlbums, selectAlbumDetail } =
+  musicSlice.selectors;
 //
 export const musicReducer = musicSlice.reducer;
 // Helper functions
-// , {
-//   next: { revalidate: 10 },
-// }
-async function getMusic() {
+
+const getAllMusic = async () => {
   const response = await fetch("http://localhost:3000/api/music/get-albums");
   const data = await response.json();
   return data;
-}
+};
+
+const getAlbumDetails = async (id: string) => {
+  const response = await fetch(`/api/music/get-details?id=${id}`);
+  const data = await response.json();
+
+  const album = mapAlbumDetail(data.data);
+  console.log(album);
+  return album;
+};
+
+const mapAlbumDetail = (item: any) => {
+  const {
+    id,
+    name,
+    artists: artistArray,
+    images: [, { url: image }],
+    external_urls: { spotify: spotifyLink },
+    release_date,
+    tracks: { items },
+  }: any = item;
+  const tracks = items.map((arrayItem: Artists) => arrayItem.name);
+  const artists = artistArray.map((arrayItem: Artists) => ({
+    name: arrayItem.name,
+    spotifyUrl: arrayItem.external_urls.spotify,
+  }));
+
+  const selectedItem: AlbumDetail = {
+    id,
+    name,
+    artists,
+    spotify_link: spotifyLink,
+    image,
+    release_date,
+    tracks,
+    category: "Music",
+  };
+
+  return selectedItem;
+};
+
 export const selectMusicPreview = createSelector(
   selectAllAlbums,
   (arr: Album[]) => {
