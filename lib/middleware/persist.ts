c28fa39488dfb7941c389ad4d884417ef81da4f2
Roy Adams <roy@eden-grafix.co.uk>
2024-10-02 14:53:39 +0100

    Added | Debounce time, so have less calls to the API

--- lib/middleware/persist.ts
+++ lib/middleware/persist.ts
@@ -3,6 +3,21 @@ import { createListenerMiddleware } from "@reduxjs/toolkit";
 import { appConfig } from "@/shared/constants/appConfig";
 import { GET_SET_DATA_API } from "@/shared/constants/urls";
 
+let debounceTimeout: NodeJS.Timeout | null = null;
+const delayTime = 500;
+
+if (typeof window !== "undefined") {
+  window.addEventListener("beforeunload", () => {
+    stopTimer();
+  });
+}
+
+const stopTimer = () => {
+  if (debounceTimeout) {
+    clearTimeout(debounceTimeout);
+  }
+};
+
 const persistStoreClientSide = (state: string) => {
   if (typeof window !== "undefined") {
     try {
@@ -16,8 +31,7 @@ const persistStoreClientSide = (state: string) => {
 export const persisterMiddleware = createListenerMiddleware();
 persisterMiddleware.startListening({
   predicate: (action, currState: any, prevState: any) => {
-    // Only persist if the state has changed
-    if (JSON.stringify(currState) !== JSON.stringify(prevState)) {
+    if (currState.uiData !== prevState.uiData) {
       persistStoreClientSide(JSON.stringify(currState));
       return true;
     }
@@ -30,27 +44,31 @@ persisterMiddleware.startListening({
       uiData: { sessionId },
     } = state;
 
-    if (sessionId) {
-      try {
-        const res = await fetch(
-          `${appConfig.url.BASE_URL}/${GET_SET_DATA_API}/category-set-data`,
-          {
-            method: "POST",
-            credentials: "include",
-            headers: {
-              "Content-Type": "application/json",
-              Cookie: `sessionId=${sessionId}`,
-            },
-            body: JSON.stringify({ state }),
+    stopTimer();
+
+    debounceTimeout = setTimeout(async () => {
+      if (sessionId) {
+        try {
+          const res = await fetch(
+            `${appConfig.url.BASE_URL}/${GET_SET_DATA_API}/category-set-data`,
+            {
+              method: "POST",
+              credentials: "include",
+              headers: {
+                "Content-Type": "application/json",
+                Cookie: `sessionId=${sessionId}`,
+              },
+              body: JSON.stringify({ state }),
+            }
+          );
+          if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
           }
-        );
-        if (!res.ok) {
-          throw new Error(`HTTP error! Status: ${res.status}`);
+        } catch (error) {
+          console.error(`There was an error: ${error}`);
+          throw error;
         }
-      } catch (error) {
-        console.error(`There was an error: ${error}`);
-        throw error;
       }
-    }
+    }, delayTime);
   },
 });
