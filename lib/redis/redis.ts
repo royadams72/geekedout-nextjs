9acef17a5fb93ffd0ea70795d5c7c717cbeeaaf2
Roy Adams <roy@eden-grafix.co.uk>
2024-11-26 16:56:56 +0000

    updated to use lamda with RestAPI

--- lib/redis/redis.ts
+++ lib/redis/redis.ts
@@ -10,23 +10,62 @@ import { CategoryType } from "@/shared/enums/category-type.enum";
 // const redis = new Redis(redis_url);
 // Updated redis host in env var2
 const redis = new Redis({
-  host: process.env.REDIS_HOST,
+  host: process.env.REDIS_HOST || "localhost",
   port: Number(process.env.REDIS_PORT) || 6379,
   password: process.env.REDIS_PASSWORD || undefined,
   tls: process.env.NODE_ENV === "production" ? {} : undefined,
 });
 
+type LambdaResponse = {
+  status: string;
+  message?: string;
+  data?: any;
+};
+
+const API_GATEWAY_URL = process.env.NEXT_PUBLIC_API_GATEWAY_URL as string;
+
+const invokeLambdaFunction = async (
+  action: string,
+  key: string,
+  value: any = null
+): Promise<LambdaResponse> => {
+  try {
+    const response = await fetch(API_GATEWAY_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ action, key, value }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data: LambdaResponse = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error invoking Lambda function:", error);
+    throw error;
+  }
+};
+
 export const saveSessionData = async (sessionId: string, data: any) => {
   const sessionTTL = 86400;
   try {
-    const response = await redis.set(
-      `session:${sessionId}`,
-      JSON.stringify(data),
-      "EX",
-      sessionTTL
-    );
+    const response = await invokeLambdaFunction("set", `session:${sessionId}`, {
+      data,
+      ttl: sessionTTL,
+    });
 
-    if (response !== "OK") {
+    // const response = await redis.set(
+    //   `session:${sessionId}`,
+    //   JSON.stringify(data),
+    //   "EX",
+    //   sessionTTL
+    // );
+
+    if (response.status !== "OK") {
       throw new Error(
         `Could not save data saveSessionData() ${process.env.REDIS_HOST}`
       );
@@ -37,8 +76,15 @@ export const saveSessionData = async (sessionId: string, data: any) => {
 };
 
 export const getSessionData = async (sessionId: string) => {
-  const data = await redis.get(`session:${sessionId}`);
-  return data ? JSON.parse(data) : null;
+  // const data = await redis.get(`session:${sessionId}`);
+  // return data ? JSON.parse(data) : null;
+  try {
+    const response = await invokeLambdaFunction("get", `session:${sessionId}`);
+    return response.data ? JSON.parse(response.data) : null;
+  } catch (error) {
+    console.error("Error retrieving session data:", error);
+    return null;
+  }
 };
 
 export const getCategoryByNameFromCache = async (
