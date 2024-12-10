212d0b7a980042e201e3b9159d672274bf760a60
Roy Adams <roy@eden-grafix.co.uk>
2024-12-10 11:40:38 +0000

    Fixed build error

--- lib/services/getCategoryFromDB.ts
+++ lib/services/getCategoryFromDB.ts
@@ -4,8 +4,8 @@ import { getStoreData } from "../db/redis";
 
 export const getCategoryFromDB = async (categoryName: string) => {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+  const sessionId = await getCookie(CookieNames.SESSION_ID);
   try {
-    const sessionId = await getCookie(CookieNames.SESSION_ID);
     if (!sessionId) {
       return { redirect: `${BASE_URL}/?redirected=true` };
     }
