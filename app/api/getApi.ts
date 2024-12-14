9faab2a50dc525662d83ebcc7b206a57c56485fb
Roy Adams <roy@eden-grafix.co.uk>
2024-12-14 09:33:49 +0000

    removed revalidation

--- app/api/getApi.ts
+++ app/api/getApi.ts
@@ -23,31 +23,31 @@ export const getApi = async (
     headers = { Authorization: `Bearer ${cookieData.access_token}` };
   }
 
-  if (!cachedData || now - lastUpdated > 120000) {
-    lastUpdated = now;
-    console.log(`Fetching fresh data for ${apiName} ...`);
-    const response = await fetch(url, {
-      method: "GET",
-      headers,
-    });
+  // if (!cachedData || now - lastUpdated > 120000) {
+  // lastUpdated = now;
+  console.log(`Fetching fresh data for ${apiName} ...`);
+  const response = await fetch(url, {
+    method: "GET",
+    headers,
+  });
 
-    const data = await response.json();
-    const returndedData = data.albums || data.data || data;
-    const res = NextResponse.json(returndedData, { status: 200 });
+  const data = await response.json();
+  const returndedData = data.albums || data.data || data;
+  const res = NextResponse.json(returndedData, { status: 200 });
 
-    if (isMusicCategory && cookieData.updated) {
-      const cookieString = await setCookieString(cookieData);
-      res?.headers.set("Set-Cookie", cookieString);
-    }
-
-    if (!response.ok) {
-      throw new ApiError(
-        response.status,
-        data.error.message || `${apiName} API error`
-      );
-    }
-    return res;
-  } else {
-    console.log(`Serving cached data...`);
+  if (isMusicCategory && cookieData.updated) {
+    const cookieString = await setCookieString(cookieData);
+    res?.headers.set("Set-Cookie", cookieString);
   }
+
+  if (!response.ok) {
+    throw new ApiError(
+      response.status,
+      data.error.message || `${apiName} API error`
+    );
+  }
+  return res;
+  // } else {
+  //   console.log(`Serving cached data...`);
+  // }
 };
