17d3b5f48f53c07eb7b208e6f9dea3bdb787acba
Roy Adams <roy@eden-grafix.co.uk>
2024-12-19 14:27:25 +0000

    test for server

--- lib/services/setComicDetailsFromDB.ts
+++ lib/services/setComicDetailsFromDB.ts
@@ -4,12 +4,15 @@ import { ComicsSliceState } from "@/lib/features/comics/comicsSlice";
 import { getCategoryFromDB } from "@/lib/services/getCategoryFromDB";
 import { isNotEmpty } from "../utils/validation";
 import { ImageNotFound } from "@/types/enums/image-not-found.enum";
+import { title } from "process";
 
 export const setComicDetailsFromDB = async (
   id: string
 ): Promise<ComicDetail | {}> => {
   try {
     const storeData = await getCategoryFromDB(CategoryType.COMICS);
+    console.log("storeData in comics", storeData);
+
     if (!storeData) {
       throw new Error("Could not get comics storeData from DB");
     }
@@ -28,7 +31,7 @@ const mapComicDetail = (
   const item: Comic | undefined = results.find(
     (comic: Comic) => comic.id?.toString() === id
   );
-
+  console.log("item.title in comics", results[0].title);
   if (!item) {
     return {};
   }
